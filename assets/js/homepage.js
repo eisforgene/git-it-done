2266b1c5afc507da27da91b97b1fff8744c58825
Bomb Atomically <ieugenelee@gmail.com>
2021-07-18 00:53:04 -0700

    capture data returned from the api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,11 @@
 var getUserRepos = function() {
-    console.log("function was called");
-};
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    response.json().then(function(data) {
+    console.log(data);
+    });
+});
+
+}
 
 getUserRepos();
+
