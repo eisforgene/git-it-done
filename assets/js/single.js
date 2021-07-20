2b49ce32154fb25d83022a14957db3d9d2b5b25f
Bomb Atomically <ieugenelee@gmail.com>
2021-07-20 00:52:44 -0700

    handle errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,9 +5,15 @@ var repoNameEl = document.querySelector("#repo-name");
 var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
-}
+    
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }   
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -22,7 +28,7 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
