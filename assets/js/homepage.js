5d5348837bb3b84ddc9f33a6507fd5c2d1e69a57
Bomb Atomically <ieugenelee@gmail.com>
2021-07-20 22:15:34 -0700

    create a function to call the github search api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,12 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl);
+};
+
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
